49d58dfd066b5b9abaa6afddf5687659d4e2623a
stsimskrad <102267093+stsimskrad@users.noreply.github.com>
2022-05-12 14:12:47 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,42 +13,42 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \DB::table('users')->insert([
-        //     'email' => 'admin@dost.com',
-        //     'password' => bcrypt('dost9ict'),
-        //     'role' => 'Super Administrator',
-        //     'is_active' => 1,
-        //     'created_at' => now(),
-        //     'updated_at' => now(),
-        //     'email_verified_at' => now(),
-        // ]);
+        \DB::table('users')->insert([
+            'email' => 'admin@dost.com',
+            'password' => bcrypt('dost9ict'),
+            'role' => 'Super Administrator',
+            'is_active' => 1,
+            'created_at' => now(),
+            'updated_at' => now(),
+            'email_verified_at' => now(),
+        ]);
 
-        // \DB::table('user_profiles')->insert([
-        //     'firstname' => 'Ra-ouf',
-        //     'lastname' => 'Jumli',
-        //     'middlename' => 'Indanan',
-        //     'gender' => 'Male',
-        //     'user_id' => 1,
-        //     'mobile' => '09557650803',
-        //     'created_at' => now(),
-        //     'updated_at' => now(),
-        // ]);
-        $this->call(UsersTableSeeder::class);
-        $this->call(UserProfilesTableSeeder::class);
-        $this->call(LocationRegionsTableSeeder::class);
-        $this->call(LocationProvincesTableSeeder::class);
-        $this->call(LocationMunicipalitiesTableSeeder::class);
-        $this->call(LocationBarangaysTableSeeder::class);
-        $this->call(ListDropdownsTableSeeder::class);
-        $this->call(ListAgenciesTableSeeder::class);
-        $this->call(ListBenefitsTableSeeder::class);
-        $this->call(ListCoursesTableSeeder::class);
-        $this->call(ListExpensesTableSeeder::class);
-        $this->call(ListProgramsTableSeeder::class);
-        $this->call(SchoolsTableSeeder::class);
-        $this->call(SchoolCampusesTableSeeder::class);
-        $this->call(SchoolCoursesTableSeeder::class);
-        $this->call(SchoolCourseProspectusesTableSeeder::class);
-        $this->call(SchoolSemestersTableSeeder::class);
+        \DB::table('user_profiles')->insert([
+            'firstname' => 'Ra-ouf',
+            'lastname' => 'Jumli',
+            'middlename' => 'Indanan',
+            'gender' => 'Male',
+            'user_id' => 1,
+            'mobile' => '09557650803',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        // $this->call(UsersTableSeeder::class);
+        // $this->call(UserProfilesTableSeeder::class);
+        // $this->call(LocationRegionsTableSeeder::class);
+        // $this->call(LocationProvincesTableSeeder::class);
+        // $this->call(LocationMunicipalitiesTableSeeder::class);
+        // $this->call(LocationBarangaysTableSeeder::class);
+        // $this->call(ListDropdownsTableSeeder::class);
+        // $this->call(ListAgenciesTableSeeder::class);
+        // $this->call(ListBenefitsTableSeeder::class);
+        // $this->call(ListCoursesTableSeeder::class);
+        // $this->call(ListExpensesTableSeeder::class);
+        // $this->call(ListProgramsTableSeeder::class);
+        // $this->call(SchoolsTableSeeder::class);
+        // $this->call(SchoolCampusesTableSeeder::class);
+        // $this->call(SchoolCoursesTableSeeder::class);
+        // $this->call(SchoolCourseProspectusesTableSeeder::class);
+        // $this->call(SchoolSemestersTableSeeder::class);
     }
 }
